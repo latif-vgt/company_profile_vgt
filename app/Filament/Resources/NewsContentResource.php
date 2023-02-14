3aef24cb99610d31953fba8a45c7855d8c045778
latief <la790x@gmail.com>
2023-02-14 14:08:54 +0700

    news content table

--- app/Filament/Resources/NewsContentResource.php
+++ app/Filament/Resources/NewsContentResource.php
@@ -69,6 +69,8 @@ class NewsContentResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
